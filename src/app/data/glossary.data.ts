b7896e8985e962d5b068b88a081eb6ff319db7e3
✅ Daniel Danielecki <daniel.danielecki@mail.com>
2025-08-25 10:40:05 +0200

    feat: add glossary data and index export for terms

---
+++ src/app/data/glossary.data.ts
@@ -0,0 +1,367 @@
+import { Term } from '../interfaces';
+
+// TODO: Think of having some kind of database for this and create a REST API call.
+export const GLOSSARY_DATA: Term[] = [
+  {
+    position: 1,
+    name: 'Advertising',
+    description:
+      'Promote Brand, Product or Service to increase awareness, engagement, interest and sales.',
+  },
+  {
+    position: 2,
+    name: 'Advisory',
+    description: 'Recommendations without taking any action.',
+  },
+  {
+    position: 3,
+    name: 'Agile',
+    description: 'Iterative and time boxed approach in software development.',
+  },
+  {
+    position: 4,
+    name: 'Automated Testing',
+    description:
+      'Method in Software Testing that makes use of certain software tools to control tests execution without or with very limited intervention from the test engineer.',
+  },
+  {
+    position: 5,
+    name: 'Application',
+    description: 'Computer program designed for End Users.',
+  },
+  {
+    position: 6,
+    name: 'Application Programming Interface (API)',
+    description:
+      'Computing interface which defines interactions between software intermediaries.',
+  },
+  {
+    position: 7,
+    name: 'Application Security',
+    description:
+      'Process of adding, developing and testing Security within Application to prevent Attack Vectors and Vulnerabilities.',
+  },
+  {
+    position: 8,
+    name: 'Artificial Intelligence (AI)',
+    description:
+      'Simulation of human intelligence performed by computer programs. It enabled computer programs adjusting to various scenarios and learning from experience.',
+  },
+  {
+    position: 9,
+    name: 'Audio Streaming',
+    description: 'Delivering real-time audio over the Internet.',
+  },
+  {
+    position: 10,
+    name: 'Automation',
+    description:
+      'Processes being performed without or with minimal human work.',
+  },
+  {
+    position: 11,
+    name: 'Backend For Frontend (BFF)',
+    description:
+      'Software Architectural Pattern built with Microservices to decouple Backend from Frontend.',
+  },
+  {
+    position: 12,
+    name: 'Backup',
+    description:
+      'Copy of data stored elsewhere with the aim to be restored if needed.',
+  },
+  {
+    position: 13,
+    name: 'Big Data',
+    description:
+      'Large sets of structured and unstructured data that is infeasible to to process using traditional databases and software techniques.',
+  },
+  {
+    position: 14,
+    name: 'Black-Box Testing',
+    description:
+      'Software Testing without knowledge what is inside an Application.',
+  },
+  {
+    position: 15,
+    name: 'Blockchain',
+    description:
+      'Blocks of Data linked with each other by usage of Cryptography.',
+  },
+  {
+    position: 16,
+    name: 'Blog',
+    description: 'Online diary or journal located on a Website.',
+  },
+  {
+    position: 17,
+    name: 'Brand',
+    description:
+      'Identifying design, logo, name, mark, sentence, slogan, symbol, term, word that distinct companies and sellers products or services.',
+  },
+  {
+    position: 18,
+    name: 'Brand Book',
+    description:
+      'Brand guide & style manual and visual identity guidelines that explains Brand Identity with presentation of Brand Standards.',
+  },
+  {
+    position: 19,
+    name: 'Brand Building',
+    description:
+      'Promotion of company\'s brand and generating awareness about it.',
+  },
+  {
+    position: 20,
+    name: 'Brand Standards',
+    description:
+      'Set of guidelines and rules for colors, fonts, logo and messaging that comprise company\'s brand.',
+  },
+  {
+    position: 21,
+    name: 'Business Acceleration',
+    description:
+      'Access to mentorship and range of support of services to become self-sufficient and stable Startup.',
+  },
+  {
+    position: 22,
+    name: 'Chatbot',
+    description:
+      'Artificial Intelligence (AI) powered computer program that simulates human conversations.',
+  },
+  {
+    position: 23,
+    name: 'Clean Code',
+    description: 'Easy to follow, read and understand Source Code.',
+  },
+  {
+    position: 24,
+    name: 'Code Review',
+    description: 'Checking, reading and viewing Source Code.',
+  },
+  {
+    position: 25,
+    name: 'Communication Protocol',
+    description: 'System of rules that allow information transmission.',
+  },
+  {
+    position: 26,
+    name: 'Competitor Profiling',
+    description:
+      'Finding out and processing data about competing businesses or products.',
+  },
+  {
+    position: 27,
+    name: 'Compliance Audit',
+    description:
+      'Unbiased evaluation and examination of internal and external rules, guidelines and laws.',
+  },
+  {
+    position: 28,
+    name: 'Computer Network',
+    description:
+      'Group of computers that user a set of common Communication Protocols.',
+  },
+  {
+    position: 29,
+    name: 'Computer Vision',
+    description:
+      'Acquiring, analysing, extracting and processing data from digital images and videos.',
+  },
+  {
+    position: 30,
+    name: 'Computing Interface',
+    description:
+      'Shared boundary across which separate components of computer system or an application can exchange information.',
+  },
+  {
+    position: 31,
+    name: 'Content Management System (CMS)',
+    description:
+      'Application to manage Digital Content without or with limited technical knowledge.',
+  },
+  {
+    position: 32,
+    name: 'Content Research',
+    description:
+      'Finding the best Digital Content for your product or service.',
+  },
+  {
+    position: 33,
+    name: 'Crowdfunding',
+    description: 'Raising small amount of money from a large audience.',
+  },
+  {
+    position: 34,
+    name: 'Cryptography',
+    description:
+      'Analysis and design of Communication Protocols for Secure Communication.',
+  },
+  {
+    position: 35,
+    name: 'Data',
+    description:
+      'Information collected through observation, in Information Technology (IT) usually represented numerically.',
+  },
+  {
+    position: 36,
+    name: 'Data Visualization',
+    description: 'Graphical representation of information.',
+  },
+  {
+    position: 37,
+    name: 'Developer',
+    description:
+      'Information Technology (IT) professional, which creates Applications and Software, also known as Programmer.',
+  },
+  {
+    position: 38,
+    name: 'Digital Content',
+    description: 'Information that exists in a digital form.',
+  },
+  {
+    position: 39,
+    name: 'Digital Crowd',
+    description: 'Online community around Crowdfunding or Crowdsourcing.',
+  },
+  {
+    position: 40,
+    name: 'Domain Name',
+    description:
+      'Identification string that defines a realm of administrative autonomy, authority or control within the Internet.',
+  },
+  {
+    position: 41,
+    name: 'Electronic Commerce (eCommerce)',
+    description: 'Buying and selling goods, products and services online.',
+  },
+  {
+    position: 42,
+    name: 'Emerging Technology',
+    description: 'New, rapidly growing and having a high impact Technology.',
+  },
+  {
+    position: 43,
+    name: 'End User',
+    description:
+      'Person who ultimately uses or is intended to ultimately use Product or Service.',
+  },
+  {
+    position: 44,
+    name: 'Forum',
+    description:
+      'Online discussion site for communication, conversations, discussions and ideas.',
+  },
+  {
+    position: 45,
+    name: 'Fuzzing',
+    description:
+      'Automated Software Testing technique that inputs invalid, random and unexpected.',
+  },
+  {
+    position: 46,
+    name: 'Gray-Box Testing',
+    description: 'Combination of Black-Box Testing and White-Box Testing.',
+  },
+  {
+    position: 47,
+    name: 'Growth Marketing',
+    description:
+      'Collection of smart Digital Marketing techniques with the aim to growth Digital Business.',
+  },
+  {
+    position: 48,
+    name: 'Human-Computer Interaction (HCI)',
+    description: 'Study how people interact with computers.',
+  },
+  {
+    position: 49,
+    name: 'Internet',
+    description: 'Global system of interconnected Computer Networks.',
+  },
+  {
+    position: 50,
+    name: 'Machine Learning (ML)',
+    description:
+      'Ability to automatically learn from experience by Computer Algorithms.',
+  },
+  {
+    position: 51,
+    name: 'Programmer',
+    description:
+      'Information Technology (IT) professional, which creates Applications and Software, also known as Developer.',
+  },
+  {
+    position: 52,
+    name: 'Quality Assurance (QA)',
+    description: 'Monitoring Software Development processes.',
+  },
+  {
+    position: 53,
+    name: 'Search Engine Optimization (SEO)',
+    description: 'Increase quantity and quality of a Web Traffic',
+  },
+  {
+    position: 54,
+    name: 'Secure Communication',
+    description:
+      'Communication without a third-party being able to listen or read',
+  },
+  {
+    position: 55,
+    name: 'Serverless',
+    description:
+      'Cloud Solution for Developers without the need of managing a Server.',
+  },
+  {
+    position: 56,
+    name: 'Startup',
+    description:
+      'Newly established company with an unique idea of product or service and aim to bring it to the market.',
+  },
+  {
+    position: 57,
+    name: 'Technology',
+    description: 'Use of scientific knowledge in practical purposes.',
+  },
+  {
+    position: 58,
+    name: 'Threat',
+    description:
+      'Possible danger that might exploit Vulnerability to breach security and therefore cause possible harm.',
+  },
+  {
+    position: 59,
+    name: 'User Interface (UI)',
+    description:
+      'Anything user may interact with to use an Application or a Website, associated with Human-Computer Interaction (HCI).',
+  },
+  {
+    position: 60,
+    name: 'Web Browser',
+    description: 'Application used to access and view Websites.',
+  },
+  {
+    position: 61,
+    name: 'Web Page',
+    description:
+      'Specific collection of information provided by a Website and displayed to an End User in a Web Browser.',
+  },
+  {
+    position: 62,
+    name: 'Web Traffic',
+    description: 'Amount of data sent and received by visitors to a Website.',
+  },
+  {
+    position: 63,
+    name: 'Website',
+    description:
+      'Collection of Web Pages and related Digital Content that is identified by a common Domain Name.',
+  },
+  {
+    position: 64,
+    name: 'White-Box Testing',
+    description:
+      'Software Testing with knowledge what is inside an Application.',
+  },
+];
