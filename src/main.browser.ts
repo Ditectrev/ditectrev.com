bcc1bbc7c71db6d994e2dfe7020f28557081d160
✅ Daniel Danielecki <daniel.danielecki@mail.com>
2026-03-16 19:17:19 +0100

    chore: remove Vercel Analytics dependency from project

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,6 +1,5 @@
 import 'hammerjs';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { inject } from '@vercel/analytics';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { enableProdMode } from '@angular/core';
@@ -9,8 +8,6 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-inject({ mode: environment.production ? 'production' : 'development' });
 
 // Workaround for service worker, issue #13351. Other than "Add to home screen" on mobile device everything works perfectly.
 document.addEventListener('DOMContentLoaded', () => {
